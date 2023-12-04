c1890a09688027fbd11108eb80cea63d1bebeb23
Kayce Basques <kayce@google.com>
2023-12-04 15:11:56 -0800

    v0.0.4

--- esfinge/__init__.py
+++ esfinge/__init__.py
@@ -1,8 +1,9 @@
 import os
 
-__version__ = '0.0.2'
-__version_info__ = (0, 0, 2)
+__version__ = '0.0.4'
+__version_info__ = (0, 0, 4)
 
 def setup(app):
     path = os.path.abspath(os.path.dirname(__file__))
     app.add_html_theme('esfinge', path)
+    return {'version': __version__, 'parallel_read_safe': True}
